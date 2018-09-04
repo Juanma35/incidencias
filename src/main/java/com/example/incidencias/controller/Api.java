63ebf20f9677c0df745b8387b8fc8ceb7d990b0d
juanma35 <tecnicohome@hotmail.es>
2018-09-04 15:28:49 +0200

    se ha creado una nueva clse

--- src/main/java/com/example/incidencias/controller/Api.java
+++ src/main/java/com/example/incidencias/controller/Api.java
@@ -1,4 +1,32 @@
 package com.example.incidencias.controller;
 
 public enum Api {
+
+    ERROR0001("No se puede conectar",0001),
+    ERROROOO2("Faltan datos",0002);
+
+    private String s;
+    private int i;
+
+  Api(String s, int i) {
+this.s=s;
+this.i=i;
+
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
 }
